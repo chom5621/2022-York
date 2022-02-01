f24c8d537cc4c51bae3ac2b06b31e561c289647f
조문선 <80917789+chom5621@users.noreply.github.com>
2022-02-01 01:29:44 -0500

    Add files via upload

---
+++ Lab_Test1.m
@@ -0,0 +1,17 @@
+A="I am a data analyst";
+
+(23+45^(1/3))/(16*0.7)+log10(589006)
+
+a=12;B=4;c=31;
+c=(a-B)+40-a/c*10
+
+x=4;y=1;z=10;
+((x>2)||(y<4))&&(z==10)
+
+a=8; h=13;
+L_ABC=sqrt((a^2)+(4*(h^2)))+((a^2)/(2*h))*log(((2*h)/a)+sqrt(((2*h)/a)^2+1))
+
+clc
+clear all
+
+factorial(20)/factorial(20-6)
