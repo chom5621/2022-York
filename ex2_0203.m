5c1c64450c80537f4f81ad8cb809d71251b1a48a
조문선 <80917789+chom5621@users.noreply.github.com>
2022-02-06 12:30:43 -0500

    Add files via upload

---
+++ ex2_0203.m
@@ -0,0 +1,10 @@
+% Input two values and output the larger one.
+x=input('Enter the first number:\n');
+y=input('Enter the second number:\n');
+disp('The larger number is');
+if (x>=y)
+    disp(x)
+else
+    disp(y)
+end
+%max(x,y)
