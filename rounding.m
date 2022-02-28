af4969da7e40f58ca1bf924a77733ffb6f124eb9
조문선 <80917789+chom5621@users.noreply.github.com>
2022-02-28 01:02:53 -0500

    Add files via upload

---
+++ rounding.m
@@ -0,0 +1,19 @@
+clc;
+A=48.36;
+B=2.4+3.6i;
+
+round(A)
+round(A,2)
+round(A,-1)
+
+round(A/0.25,0)*0.25
+round(A/0.05,0)*0.05
+round(A/0.5,0)*0.5
+
+round(B,0)
+round(B,-1)
+round(B,1)
+
+floor(B) % negative inf
+ceil(B) % positive inf
+fix(B) % close to 0
