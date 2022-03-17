9c4128d33e8985870453301aa3d0ece7ee1fee3d
조문선 <80917789+chom5621@users.noreply.github.com>
2022-03-17 00:25:52 -0400

    Add files via upload

---
+++ Lec7_concatenating_matrices.m
@@ -0,0 +1,11 @@
+X=[1 2; 3 4; 5 6]
+
+D=[9 8 7; 6 5 4; 3 2 1]
+
+F=[X D]
+%     1     2     9     8     7
+%     3     4     6     5     4
+%     5     6     3     2     1
+
+F=[X,D]
+% returns same matrix with above
