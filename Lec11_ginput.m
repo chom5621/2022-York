ad019419571b2fce6fc0d70805b18e76387ddb55
조문선 <80917789+chom5621@users.noreply.github.com>
2022-04-10 02:04:13 -0400

    Add files via upload

---
+++ Lec11_ginput.m
@@ -0,0 +1,18 @@
+[x y b]=ginput(3)
+% If I use the mouse to select 3 points in the figure window,
+% I will get an output on the command window similar to:
+
+% x = 0.1959
+%     0.2005
+%     0.2005
+% These are the x-coordinates of the three points you clicked on
+
+% y = 0.8006
+%     0.7947
+%     0.7947
+% These are the y-coordinates of the three points you clicked on
+
+% b = 1
+%     1
+%     1
+% These values means that you did not type the keyboard
