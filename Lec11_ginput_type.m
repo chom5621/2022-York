fce3187b6e8392a27bf4042c2a01a04dd97c0992
조문선 <80917789+chom5621@users.noreply.github.com>
2022-04-11 00:08:31 -0400

    Add files via upload

---
+++ Lec11_ginput_type.m
@@ -0,0 +1,21 @@
+[x y b]=ginput(3)
+% If I use the mouse to select 3 points in the figure window,
+% I will get an output on the command window similar to:
+
+% x = -0.1682
+%     -0.1682
+%     -0.1682
+% These are the x-coordinates of the cursor position.
+% The value depends on where the cursor is.
+
+% y = -0.1352
+%     -0.1352
+%     -0.1352
+% These are the y-coordinates of the cursor position.
+% The value depends on where the cursor is.
+
+% b = 97
+%     99
+%     109
+% These are the internal ASCII codes of the characters typed.
+% 97 is 'a', 99 is 'c' and 109 is 'm'.
