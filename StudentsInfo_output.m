3da829e1c9eb01d985e5c7ce95254fcacc6c99cc
조문선 <80917789+chom5621@users.noreply.github.com>
2022-04-26 05:26:43 -0400

    Delete StudentsInfo_output.m

--- StudentsInfo_output.m
+++
@@ -1,9 +0,0 @@
-T=readtable('StudentsInfo');
-t.name={'Cairo Jimmy Cook','Tresha Wilkerson','Lucas Justice','Darren Tessie Clement'};
-t.gpa={4.253,3.978,4.512,3.788};
-
-Output=fopen('Output.txt','w');
-for i=1:4
-    fprintf(Output, 'Student Name:  %s , Student GPA: %.2f.\n',t.name{i},t.gpa{i});
-end
-fclose(Output);
